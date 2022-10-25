92e76965379c1f2757df0304b01f4b473c73bdd5
John Oduor Otieno <johnoduor222@gmail.com>
2022-10-25 14:40:45 -0700

    completed

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex,color,temperament,alive) VALUES ('Mr. Chocolate', 3, 'M','White','sad',1);
+INSERT INTO bears (name, age, sex,color,temperament,alive) VALUES ('Rowdy', 13, 'M','Green','happy',1);
+INSERT INTO bears (name, age, sex,color,temperament,alive) VALUES ('Tabitha', 33, 'F','Red','sad',0);
+INSERT INTO bears (name, age, sex,color,temperament,alive) VALUES ('Sergeant Brown', 23, 'M','Brown','sad',1);
+INSERT INTO bears (name, age, sex,color,temperament,alive) VALUES ('Melissa', 31, 'F','Violet','sad',0);
+INSERT INTO bears (name, age, sex,color,temperament,alive) VALUES ('Grinch', 33, 'M','Purple','sad',1);
+INSERT INTO bears (name, age, sex,color,temperament,alive) VALUES ('Wendy', 32, 'F','Black','excited',1);
+INSERT INTO bears (name, age, sex,color,temperament,alive) VALUES (NULL, 34, 'F','Green','happy',0);
